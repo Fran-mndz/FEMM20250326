930dae746cc6dbc2656d27baab44c09ca21a505c
ericka.segura05 <ericka.segura05@hotmail.com>
2025-03-26 08:41:35 -0600

    Agregar archivos de proyecto.

---
+++ FEMM20250326.AppWebMVC/Models/Warehouse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FEMM20250326.AppWebMVC.Models;
+
+public partial class Warehouse
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "El nombre de la bodega es obligatorio")]
+    public string WarehouseName { get; set; } = null!;
+
+    public string? Notes { get; set; }
+
+    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+}
